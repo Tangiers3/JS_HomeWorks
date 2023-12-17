cc6bc6d772b64c228212da7b24d4921a394c0c84
Tangiers3 <vadym.hapiienko@gmail.com>
2023-12-16 20:57:50 -0700

    HW 2 comleted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Task #1
+
+let name = 'John';
+let admin = name;
+alert(admin);
+
+// Task #2
+
+const ourPlanet = 'The Earth';
+let visitorName = prompt('What is your name?');
+
+// Task #3
+
+let sum = 10 + '10';
+console.log(typeof (sum));
+console.log(sum);
+//When you concatenate a string with a number in JavaScript, the string gets concatenated with the string representation of the number. In this case, the string "10" is concatenated with the number 10, resulting in the string "1010". Therefore, the variable's type becomes "string"
