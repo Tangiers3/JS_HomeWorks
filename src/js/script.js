88995a275e3878f4c8aa521dcc0b077d336907e4
Tangiers3 <vadym.hapiienko@gmail.com>
2023-12-16 21:22:28 -0700

    HW 3 comleted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let firstNumber = prompt('Please type your first number:');
+let secondNumber = prompt('Please type your second number:')
+
+firstNumber = parseInt(firstNumber);
+secondNumber = parseInt(secondNumber);
+
+console.log('Compilation result:', firstNumber, "+", secondNumber, '=', firstNumber + secondNumber );
+console.log('Subtraction result:', firstNumber, "-", secondNumber, '=', firstNumber - secondNumber );
+console.log('Multiplication result:', firstNumber, "*", secondNumber, '=', firstNumber * secondNumber );
+console.log('Division result:', firstNumber, "/", secondNumber, '=', firstNumber / secondNumber );
