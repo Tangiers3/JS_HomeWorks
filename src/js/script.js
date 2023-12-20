63596eb0c46005ec4cb2de15dd4c841856de6c08
Tangiers3 <vadym.hapiienko@gmail.com>
2023-12-19 22:27:13 -0700

    HW 4 comleted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+// Task #1
+/*
+const firstInput = prompt('Please enter the first value');
+const secondInput = prompt('Please enter the second value');
+const thirdInput = prompt('Please enter the third value');
+
+const inputResult = firstInput  + ' ' + secondInput + ' ' + thirdInput;
+
+console.log(inputResult);*/
+
+
+// Task #2
+
+/*
+const inputNumber = prompt('Please enter a five-digit number');
+
+const inputResult = `${inputNumber[0]} ${inputNumber[1]} ${inputNumber[2]} ${inputNumber[3]} ${inputNumber[4]}`;
+console.log(inputResult);*/
