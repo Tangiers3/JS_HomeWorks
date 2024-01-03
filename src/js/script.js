711e516b2ad7fd0c7e99fda5c4f39039985d692c
Tangiers3 <vadym.hapiienko@gmail.com>
2024-01-03 09:29:42 -0700

    HW 12 comleted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+const firstNumber = +prompt('Enter your first number');
+const secondNumber = +prompt('Enter your second number');
+
+const sum = firstNumber + secondNumber;
+const sub = firstNumber - secondNumber;
+const mult = firstNumber * secondNumber;
+const dev = firstNumber / secondNumber;
+
+const result =
+    firstNumber + ' + ' + secondNumber + ' = ' + sum + '\n' +
+    firstNumber + ' - ' + secondNumber + ' = ' + sub + '\n' +
+    firstNumber + ' * ' + secondNumber + ' = ' + mult + '\n' +
+    firstNumber + ' / ' + secondNumber + ' = ' + dev + '\n';
+
+alert(result);
