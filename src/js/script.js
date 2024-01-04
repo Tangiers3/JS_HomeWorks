92c4de599514c9325979ac9f662332bb33ed27d2
Tangiers3 <vadym.hapiienko@gmail.com>
2024-01-04 12:49:57 -0700

    HW 15 comleted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const firstNumber = +prompt('Enter the first number');
+const secondNumber = +prompt('Enter the second number');
+const thirdNumber = +prompt('Enter the third number');
+
+const average = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert('Еhe average of the three values you entered is ' + average);
+
+
+
