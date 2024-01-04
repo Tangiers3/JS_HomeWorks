8877e2100362ecae15335c6c88f58b13fb27121e
Tangiers3 <vadym.hapiienko@gmail.com>
2024-01-04 13:30:10 -0700

    HW 13 comleted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const action = prompt('Please, choose one of the operations (add, sub, mult, div)');
+const firstNumber = +prompt('Enter the first number');
+const secondNumber = +prompt('Enter the second number');
+
+const addResult = Number(firstNumber + secondNumber);
+const subResult = Number(firstNumber - secondNumber);
+const multResult = Number(firstNumber * secondNumber);
+const divResult = Number(firstNumber / secondNumber);
+
+switch (action){
+    case 'add':
+        alert(`${firstNumber} + ${secondNumber} = ${ addResult}`);
+        break;
+    case 'sub':
+        alert(`${firstNumber} - ${secondNumber} = ${ subResult}`);
+        break;
+    case 'mult':
+        alert(`${firstNumber} * ${secondNumber} = ${ multResult}`);
+        break;
+    case 'div':
+        if (secondNumber === 0) {
+            alert('Division by zero is not allowed');
+        } else {
+            alert(`${firstNumber} / ${secondNumber} = ${divResult}`);
+        }
+        break;
+
+}
+
+
+
