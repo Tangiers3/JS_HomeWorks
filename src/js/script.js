58699f8c7ecc547968e79be862a0636e603dd7d1
Tangiers3 <vadym.hapiienko@gmail.com>
2024-01-10 23:01:40 -0700

    HW 23 comleted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+let sumOfSquares = 0
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    sumOfSquares += Math.pow(arr[i],2);
+}
+console.log(sum);
+console.log(sumOfSquares);
+
+
+
