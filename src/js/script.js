719ce5b2c97652decf24f4cac4b76350e83b1920
Tangiers3 <vadym.hapiienko@gmail.com>
2024-02-05 16:34:40 -0700

    hw 26 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const generateKey = function generateKey(length, characters) {
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i += 1) {
+    const index = Math.floor(Math.random() * charactersLength);
+    result += characters[index];
+  }
+  return result;
+};
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
