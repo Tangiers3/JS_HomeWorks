9df0367734dd9e7a513cdc5a486b953c456744c9
Tangiers3 <vadym.hapiienko@gmail.com>
2024-02-05 17:13:19 -0700

    hw 27 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+/* eslint-disable no-alert */
+
+const numb = +prompt('Введите число, которое хотите возвести в степень');
+const exponent = +prompt('Ваедите степень: ') || 1;
+
+const raiseToPower = function raiseToPower(number, power = 1) {
+  if (Number.isNaN(number) || Number.isNaN(power)) {
+    return 'Одно из введенных значений не является числом';
+  }
+  return number ** power;
+};
+const result = raiseToPower(numb, exponent);
+alert(result);
