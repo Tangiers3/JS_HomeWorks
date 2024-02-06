a73fbf7113f78dc5424e5f31f227e4ca65a25e29
Tangiers3 <vadym.hapiienko@gmail.com>
2024-02-05 20:54:11 -0700

    hw 29 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+/* eslint-disable no-console */
+
+const positiveNumbers = function positiveNumbers(arr) {
+  if (arr.length === 0) {
+    return 'Массив пуст';
+  }
+  const positiveArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > 0) {
+      positiveArr.push(arr[i]);
+    }
+  }
+  return positiveArr.length > 0 ? positiveArr : null;
+};
+const arr = [1, 2, 3, -1, -2, -3];
+const positiveArr = positiveNumbers(arr);
+console.log(positiveArr);
