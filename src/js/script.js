2ea13676457f03f9ceda77456aa5d926bb6ef049
Tangiers3 <vadym.hapiienko@gmail.com>
2024-02-21 09:36:25 -0700

    hw 24 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// eslint-disable-next-line no-alert
+/* eslint-disable no-alert */
+
+const number = +prompt('Введите число');
+if (number < 1) {
+  // eslint-disable-next-line no-console
+  console.log('NaN');
+} else {
+  for (let i = 2; i <= number; i += 1) {
+    if (number % 2 === 0) {
+      // eslint-disable-next-line no-console
+      console.log(i);
+      break;
+    }
+  }
+}
