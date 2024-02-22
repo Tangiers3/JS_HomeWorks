63a80cc890c1a0d79dd86607f576cd8d893d09ca
Tangiers3 <vadym.hapiienko@gmail.com>
2024-02-22 14:25:34 -0700

    hw 34 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+'use strict';
+
+const greet = function greet() {
+  console.log(`Hello, my name is ${this.name}`);
+};
+const createUser = function createUser(name, age) {
+  const user = {
+    name,
+    age,
+    greet,
+
+  };
+  return user;
+};
+const user1 = createUser('Oleg', 30);
+user1.greet();
+const user2 = createUser('Tolik', 46);
+user2.greet();
