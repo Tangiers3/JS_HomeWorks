31767c156b81e1f0a51018b8b163d840b5f3e6bc
Tangiers3 <vadym.hapiienko@gmail.com>
2024-02-22 12:15:59 -0700

    hw 30 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const costCalculate = function costCalculate(litersOfBorsch) {
+  const potatoPerLiter = 4;
+  const potatoWeight = 75;
+  const priceKg = 13;
+
+  const totalWeightInGrams = litersOfBorsch * potatoPerLiter * potatoWeight;
+
+  const totalWeightInKg = Math.ceil(totalWeightInGrams / 1000);
+  const totalCost = totalWeightInKg * priceKg;
+  return totalCost;
+};
+const cost = costCalculate(48);
+console.log(`Стоимость картофеля: ${cost} грн. `);
