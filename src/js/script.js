e2d1446935bcb3214df05dc97cc50a073f2e4707
Tangiers3 <vadym.hapiienko@gmail.com>
2024-02-22 14:40:07 -0700

    hw 51 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+/* eslint-disable no-console */
+const parseJSON = function parseJSON(string) {
+  try {
+    return JSON.parse(string);
+  } catch (err) {
+    throw new Error('Invalid JSON string');
+  }
+};
+console.log(parseJSON('{"name": "John", "age": 30}'));
+console.log(parseJSON('{"name": "John", "age":'));
