31e2a0d2141d4b95369c2aba2c2fac3c9d51f451
Tangiers3 <vadym.hapiienko@gmail.com>
2024-03-12 20:32:57 -0600

    hw 52 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const items = document.querySelectorAll('li');
+const itemsArr = [];
+
+for (const item of items) {
+  console.log(item);
+  itemsArr.push(item.textContent);
+}
+console.log(`Общее количесвто элементов: ${items.length}`);
+console.log(itemsArr);
