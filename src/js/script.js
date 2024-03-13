4f58b00708c86bf302ebd49fa3518ef7653f7638
Tangiers3 <vadym.hapiienko@gmail.com>
2024-03-12 21:52:50 -0600

    hw 53 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+/* eslint-disable no-console */
+
+const lis = document.querySelectorAll('li');
+// eslint-disable-next-line no-restricted-syntax
+for (const li of lis) {
+  console.log(li.textContent);
+}
+const ul = document.getElementById('ulId');
+const attrs = ul.attributes;
+
+const values = [];
+const names = [];
+// eslint-disable-next-line no-restricted-syntax
+for (const attr of attrs) {
+  values.push(attr.value);
+  names.push(attr.name);
+}
+console.log(values);
+console.log(names);
+
+ul.lastElementChild.textContent = 'Привет, меня зовут Вадим';
+ul.firstElementChild.setAttribute('data-my-name', 'Name');
+
+ul.removeAttribute('data-dog-tail');
